40bd13c6497e7a13114b86f5f0b2089dac4cf09e
rustmn <allorim@protonmail.com>
2023-02-01 01:48:45 +0300

    i

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { Player, Controller } from 'dm-standard';
+
+const types = {
+  dash: 'application/dash+xml',
+  hls: 'application/x-mpegURL'
+}
+
+const videos = [
+  {
+    id: `-en`,
+    language: 'en',
+    captions: {
+      src: 'https://cdn.player-standart-aws.deep.movie/out/INTRO_NI/av1/en/captions/INTRO_NI_ENG.vtt',
+      language: 'en'
+    },
+    video: {
+      hls: {
+        src: 'https://cdn.player-standart-aws.deep.movie/NI_NEW/NI_EN/H265/index.m3u8',
+        type: types.hls
+      },
+      dash: {
+        src: 'https://cdn.player-standart-aws.deep.movie/NI_NEW/NI_EN/AV1/stream.mpd',
+        type: types.dash,
+      }
+    }
+  },
+  {
+    id: `-ru`,
+    language: 'ru',
+    captions: {
+      src: 'https://cdn.player-standart-aws.deep.movie/out/INTRO_NI/av1/ru/captions/INTRO_NI_RUS.vtt',
+      language: 'ru'
+    },
+    video: {
+      hls: {
+        src: 'https://cdn.player-standart-aws.deep.movie/NI_NEW/NI_RU/H265/index.m3u8',
+        type: types.hls
+      },
+      dash: {
+        src: 'https://cdn.player-standart-aws.deep.movie/NI_NEW/NI_RU/AV1/stream.mpd',
+        type: types.dash,
+      }
+    }
+  }
+];
+
 
 function App() {
+  const pause = () => {
+    Controller.pause();
+  }
+  const play = () => {
+    Controller.play();
+  }
+  const fs = () => {
+    Controller.toggleFullscreen();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <button onClick = {pause}>Pause</button>
+    <button onClick = {play}>Play</button>
+    <button onClick = {fs}>FS</button>
+    <div style = {{
+      width: '80vw',
+      height: '60vh'
+    }}>
+
+      <Player videos = { videos } />
     </div>
+    </>
   );
 }
 
